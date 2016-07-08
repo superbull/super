9647e60ba7cccd38c0dc615579cdce11557f825d
superbull <superbulldeng@gmail.com>
2016-07-08 15:09:11 +0800

    add logger into AbstractRepository

--- src/Repository/AbstractRepository.php
+++ src/Repository/AbstractRepository.php
@@ -1,15 +1,18 @@
 <?php
 namespace Superbull\Super\Repository;
 
+use Psr\Log\LoggerInterface;
 use Aura\Sql\ExtendedPdo;
 
 abstract class AbstractRepository
 {
+    protected $logger;
     protected $pdo;
 
     public function __construct(
         ExtendedPdo $pdo
     ) {
+        $this->logger = $logger;
         $this->pdo = $pdo;
     }
 }
